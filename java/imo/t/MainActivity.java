975c851e8967119942ba3f8978b295926eabb35c
IMOitself <russjonn@gmail.com>
2025-01-30 10:46:44 +0800

    feat: add and test font
    jetbrains mono regular

--- java/imo/t/MainActivity.java
+++ java/imo/t/MainActivity.java
@@ -1,8 +1,13 @@
 package imo.t;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		
+		TextView testText = findViewById(R.id.test_text);
+		testText.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/jetbrainsmonoregular.ttf"));
     }
 }
